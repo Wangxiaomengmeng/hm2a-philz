bfee1cacb961fe04c8c792de7d111e128156c930
Wangxiaomengmeng <2242658465@qq.com>
2026-09-13 10:14:05 +0800

    创建 device/xiaomi/hm2a/cm_hm2a.mk

---
+++ device/xiaomi/hm2a/cm_hm2a.mk
@@ -0,0 +1,8 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+PRODUCT_NAME := cm_hm2a
+PRODUCT_DEVICE := hm2a
+PRODUCT_BRAND := xiaomi
+PRODUCT_MODEL := hm2a
+PRODUCT_MANUFACTURER := xiaomi
